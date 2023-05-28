095e0eba3820e5440145b15ec626e1e2dcab824d
Andrey Baranov <baranov.a.64103@gmail.com>
2023-05-29 00:27:44 +0200

    bug fix chart

--- src/components/Chart/Chart.jsx
+++ src/components/Chart/Chart.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useMemo } from 'react';
 import { Doughnut } from 'react-chartjs-2';
 import {
   Chart as ChartJS, ArcElement, Tooltip, Legend,
@@ -15,49 +15,73 @@ function Chart({ dataExpense }) {
   // const chartColors = ['green', 'red'];
 
   // bug that renders the chart text multiple times in a second (bad performance)
-  function getSum(arrData) {
-    const sum = arrData.reduce((a, b) => a + b, 0);
-    console.log(sum);
-    return sum;
-  }
+  // function getSum(arrData) {
+  //   const sum = arrData.reduce((a, b) => a + b, 0);
+  //   console.log(sum);
+  //   console.log(dataAmount);
+  //   return sum;
+  // }
+
+  // render once or when the component is mounted
+  const sum = useMemo(() => dataAmount.reduce((a, b) => {
+    console.log(dataAmount);
+    return a + b;
+  }, 0), [dataAmount]);
 
   const centerText = {
     id: 'centerText',
-    beforeDatasetsDraw(chart, args, options) {
-      const { ctx, data } = chart;
+    beforeDatasetsDraw(chart) {
+      const { ctx } = chart;
 
       ctx.save();
       ctx.font = 'bolder 30px Arial'; // font size string value
       ctx.fillStyle = 'black';
       ctx.textAlign = 'center';
       ctx.textBaseline = 'middle';
-      ctx.fillText(`Total: ${getSum(dataAmount)}`, chart.getDatasetMeta(0).data[0].x, chart.getDatasetMeta(0).data[0].y);
+      ctx.fillText(`Total: BAM ${sum}`, chart.getDatasetMeta(0).data[0].x, chart.getDatasetMeta(0).data[0].y);
       ctx.restore();
     },
   };
 
+  console.log('label data', expensesLabels);
+
   // def a chart parameters
   const data = {
-    label: expensesLabels,
+    labels: expensesLabels,
     datasets: [{
-      label: expensesLabels,
       data: dataAmount,
       backgroundColor: expensesChartColors,
       borderColor: expensesChartColors,
       cutout: '75%',
     }],
   };
-  //   const options = {};
+  const options = {
+    scales: {
+      yAxes: [{
+        ticks: {
+          min: 0,
+          max: 1,
+        },
+      }],
+      xAxes: [{
+        ticks: {
+          callback: (v, i) => (i + 1 <= expensesLabels.length ? v : null),
+        },
+      }],
+    },
+  };
+
   return (
     <div>
       <Doughnut
         data={data}
+        options={options}
         plugins={[centerText]}
       />
     </div>
   );
 }
-
+// just validate the data type of the props for the eslint
 Chart.propTypes = {
   dataExpense: PropTypes.shape({
     dataAmount: PropTypes.arrayOf(PropTypes.number).isRequired,
