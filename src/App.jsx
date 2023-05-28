c3a7821dfeaa8290e06de304b57428b8ab329f47
Andrey Baranov <baranov.a.64103@gmail.com>
2023-05-28 17:49:48 +0200

    fixed chart component and added css

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css';
+import React from 'react';
+import Expenses from './components/Expenses/Expenses';
+
+function App() {
+  return (
+    <div className="App">
+      <Expenses />
+    </div>
+  );
+}
+
+export default App;
