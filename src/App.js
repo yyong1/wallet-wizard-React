3488daa0604a06ffd6c593a55527fda9c086aaea
rania-w <rania.weiss@stu.ibu.edu.ba>
2023-05-30 23:41:36 +0200

    adding routes

--- src/App.js
+++ src/App.js
@@ -2,16 +2,21 @@ import logo from './logo-walletwizard-small.png';
 // import './App.css';
 import Homepage from './components/homepage/Homepage';
 import Navbar from './components/navbar/Navbar';
+import { Route, Routes } from 'react-router-dom';
+import Registration from './components/registration/Registration';
+import Login from './components/login/Login';
 
 function App() {
   return (
     <div className="App">
-        <Navbar />
-        <div>
-          <Homepage />
-          
-        </div>
-        
+      <Navbar />
+      <div>
+        <Routes>
+          <Route path='/' element={<Homepage />} />
+          <Route path='/register' element={<Registration />} />
+          <Route path='/login' element={<Login />} />
+        </Routes>
+      </div>
     </div>
   );
 }
