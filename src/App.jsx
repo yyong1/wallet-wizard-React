69c5b209481ae995c091ca31b466592d8d203454
rania-w <rania.weiss@stu.ibu.edu.ba>
2023-06-04 11:03:30 +0200

    breaking changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,28 @@
-import './App.css';
-import React from 'react';
+import logo from './logo-walletwizard-small.png';
+// import './App.css';
+import Homepage from './components/homepage/Homepage';
+import Navbar from './components/navbar/Navbar';
+import { Route, Routes } from 'react-router-dom';
+import Registration from './components/registration/Registration';
+import Login from './components/login/Login';
+import Categories from './components/categories/Categories';
+import Accounts from './components/accounts/Accounts';
 import Expenses from './components/Expenses/Expenses';
 
 function App() {
   return (
     <div className="App">
-      <Expenses />
+      <Navbar />
+      <div>
+        <Routes>
+          <Route path='/' element={<Homepage />} />
+          <Route path='/register' element={<Registration />} />
+          <Route path='/login' element={<Login />} />
+          <Route path='/categories' element={<Categories />}/>
+          <Route path='/accounts' element={<Accounts />}/>
+          <Route path='/expenses' element={<Expenses />}/>
+        </Routes>
+      </div>
     </div>
   );
 }
